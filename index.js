1b6f2dafb1d8be5bb400205c630288ce56ce5605
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-05 04:28:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
     if(array[i] === findFn()) {
-      return array[i];
+      return findFn();
     }
   }
   return null;
